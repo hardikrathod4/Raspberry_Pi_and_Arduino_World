175cf081d4560229d88626bdeff32f520920da37
Hardik Rathod <hardikrathod4@gmail.com>
2015-11-02 15:49:55 +0100

    Create Motion_Sensor.py

---
+++ Raspberry_Pi/GPIO_ZERO_PROJECTS/Motion_Sensor.py
@@ -0,0 +1,10 @@
+
+# Turn ON the LED when Motion detected...
+
+from gpiozero import MotionSensor, LED
+
+pir = MotionSensor(5)
+led = LED(16)
+
+pir.when_motion = led.on
+pir.when_no_motion = led.off
