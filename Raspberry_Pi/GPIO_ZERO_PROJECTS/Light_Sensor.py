40227f39f95693f86cbfc5cee013475d36e79531
Hardik Rathod <hardikrathod4@gmail.com>
2015-11-02 15:56:41 +0100

    Create Light_Sensor.py

---
+++ Raspberry_Pi/GPIO_ZERO_PROJECTS/Light_Sensor.py
@@ -0,0 +1,12 @@
+
+# Displays It's Light or Dark
+
+from gpiozero import LightSensor
+
+sensor = LightSensor(18)
+
+while True:
+    sensor.wait_for_light()
+    print("It's light...")
+    sensor.wait_for_dark()
+    print("It's dark...")
