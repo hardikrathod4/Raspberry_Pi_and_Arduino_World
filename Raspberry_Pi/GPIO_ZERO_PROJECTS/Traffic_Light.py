32a6dca984daf50ca48f43151dab1280cc28b87c
Hardik Rathod <hardikrathod4@gmail.com>
2015-11-02 12:57:19 +0100

    Create Traffic_Light.py

---
+++ Raspberry_Pi/GPIO_ZERO_PROJECTS/Traffic_Light.py
@@ -0,0 +1,27 @@
+
+# Traffic light system....
+
+from gpiozero import LED
+from time import sleep
+
+red = LED(2)
+yellow = LED(3)
+green = LED(4)
+
+green.off()
+yellow.off()
+red.off()
+
+while True:
+              sleep(10)
+              green.off()
+              yellow.on()
+              sleep(1)
+              yellow.off()
+              red.on()
+              sleep(10)
+              yellow.on()
+              sleep(1)
+              green.on()
+              yellow.off()
+              red.off()
