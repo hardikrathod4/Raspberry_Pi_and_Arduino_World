5334879c9209698c594cd16378141d3fc6ad0f09
Hardik Rathod <hardikrathod4@gmail.com>
2015-11-02 16:15:35 +0100

    Update Button_Controlled_Robot.py

--- Raspberry_Pi/GPIO_ZERO_PROJECTS/Button_Controlled_Robot.py
+++ Raspberry_Pi/GPIO_ZERO_PROJECTS/Button_Controlled_Robot.py
@@ -13,16 +13,16 @@ right = Button(16)
 fw = Button(21)
 bw = Button(20)
 
-fw.when_pressed = robot.forward
+fw.when_pressed = robot.forward    # Moves frwd until you release the fw button
 fw.when_released = robot.stop
 
-left.when_pressed = robot.left
+left.when_pressed = robot.left     # Turns left until you release the left button
 left.when_released = robot.stop
 
-right.when_pressed = robot.right
+right.when_pressed = robot.right   # Turns right you release the right button
 right.when_released = robot.stop
 
-bw.when_pressed = robot.backward
+bw.when_pressed = robot.backward   # Moves backwd until you release the bw button
 bw.when_released = robot.stop
 
 pause()
